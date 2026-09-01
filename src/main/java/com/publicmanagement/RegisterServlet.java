ca934dc0fa0fab0e1fb9128bd02e13853fc2a825
Deepika <gompadeepika03@gmail.com>
2026-09-01 23:55:17 +0530

    Fix Railway deployment

--- src/main/java/com/publicmanagement/RegisterServlet.java
+++ src/main/java/com/publicmanagement/RegisterServlet.java
@@ -1,5 +1,9 @@
 package com.publicmanagement;
-
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
