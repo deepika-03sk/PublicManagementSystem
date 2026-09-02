559624f73e5010f97de3d4dbec30bf8eaced09b0
Deepika <gompadeepika03@gmail.com>
2026-09-03 00:49:50 +0530

    Test database connection

--- src/main/java/com/publicmanagement/DBConnection.java
+++ src/main/java/com/publicmanagement/DBConnection.java
@@ -13,17 +13,11 @@ public class DBConnection {
         String user = System.getenv("MYSQLUSER");
         String password = System.getenv("MYSQLPASSWORD");
 
-        System.out.println("=== DATABASE TEST ===");
-        System.out.println("MYSQLHOST: " + host);
-        System.out.println("MYSQLPORT: " + port);
-        System.out.println("MYSQLDATABASE: " + database);
-        System.out.println("MYSQLUSER: " + user);
-        System.out.println("PASSWORD EXISTS: " + (password != null));
-
-        if (host == null || port == null || database == null
-                || user == null || password == null) {
-            throw new Exception("MYSQL variables are missing");
-        }
+        System.out.println("MYSQLHOST = " + host);
+        System.out.println("MYSQLPORT = " + port);
+        System.out.println("MYSQLDATABASE = " + database);
+        System.out.println("MYSQLUSER = " + user);
+        System.out.println("PASSWORD EXISTS = " + (password != null));
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
@@ -33,8 +27,6 @@ public class DBConnection {
                 + "&serverTimezone=UTC"
                 + "&connectTimeout=10000"
                 + "&socketTimeout=10000";
-
-        System.out.println("Connecting to Railway MySQL...");
 
         return DriverManager.getConnection(url, user, password);
     }
