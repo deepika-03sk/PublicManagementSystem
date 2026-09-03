d335e12402e4caf13a4c4b40703716e6390283ab
Deepika <gompadeepika03@gmail.com>
2026-09-03 09:19:52 +0530

    Fix Railway database connection

--- src/main/java/com/publicmanagement/DBConnection.java
+++ src/main/java/com/publicmanagement/DBConnection.java
@@ -7,19 +7,18 @@ public class DBConnection {
 
     public static Connection getConnection() throws Exception {
 
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
         String host = System.getenv("MYSQLHOST");
         String port = System.getenv("MYSQLPORT");
         String database = System.getenv("MYSQLDATABASE");
         String user = System.getenv("MYSQLUSER");
         String password = System.getenv("MYSQLPASSWORD");
 
-        System.out.println("MYSQLHOST = " + host);
-        System.out.println("MYSQLPORT = " + port);
-        System.out.println("MYSQLDATABASE = " + database);
-        System.out.println("MYSQLUSER = " + user);
-        System.out.println("PASSWORD EXISTS = " + (password != null));
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        if (host == null || port == null || database == null
+                || user == null || password == null) {
+            throw new Exception("Railway MySQL variables are missing.");
+        }
 
         String url = "jdbc:mysql://" + host + ":" + port + "/" + database
                 + "?useSSL=false"
