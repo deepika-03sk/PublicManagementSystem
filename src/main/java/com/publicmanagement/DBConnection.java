6120383550877aebb00a4455067bd098c77527cb
Deepika <gompadeepika03@gmail.com>
2026-09-17 11:18:26 +0530

    Enable MySQL TLS connection

--- src/main/java/com/publicmanagement/DBConnection.java
+++ src/main/java/com/publicmanagement/DBConnection.java
@@ -21,8 +21,7 @@ public class DBConnection {
         }
 
         String url = "jdbc:mysql://" + host + ":" + port + "/" + database
-                + "?useSSL=false"
-                + "&allowPublicKeyRetrieval=true"
+                + "?sslMode=REQUIRED"
                 + "&serverTimezone=UTC"
                 + "&connectTimeout=15000"
                 + "&socketTimeout=15000";
